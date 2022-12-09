565292a15fe17c19f3382622486d6a409fe5c9bb
Andrew <59783848+Excidious@users.noreply.github.com>
2022-12-08 19:44:34 -0500

    Add offline message

--- bot.js
+++ bot.js
@@ -8,16 +8,23 @@ client.on('ready', () => {
 client.on('presenceUpdate', (oldPresence, newPresence) => {
   const user = newPresence.user;
 
-  // Check if the user is the one we want to ping
-  if (user.id === '256213318060998666') {
-    // Check if the user was offline and is now online
-    if (oldPresence.status === 'offline' && newPresence.status === 'online') {
+    // Check if the user is the one we want to ping
+    if (user.id === '256213318060998666') {
 
-      // Get the channel to send the message in
-      const channel = client.channels.cache.get('349096556818857995');
+      // Check if the user was offline and is now online
+      if (oldPresence.status === 'offline' && newPresence.status === 'online') {
+          // Get the channel to send the message in
+          const channel = client.channels.cache.get('349096556818857995');
 
-      // Send the message
-      channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696440`);
+          // Send the message
+          channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696440`);
+        } 
+      else if (oldPresence.status === 'online' && newPresence.status === 'offline') {
+          // Get the channel to send the message in
+          const channel = client.channels.cache.get('349096556818857995');
+      
+          // Send the message
+          channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696441`);
     }
   }
 });
