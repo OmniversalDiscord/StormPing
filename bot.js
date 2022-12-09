1789a67290a21ec97edf9efaca201a6dfb12e09e
Andrew <59783848+Excidious@users.noreply.github.com>
2022-12-09 00:32:05 -0500

    Post to storm echo chamber

--- bot.js
+++ bot.js
@@ -15,21 +15,21 @@ client.on('presenceUpdate', (oldPresence, newPresence) => {
 
         if (oldPresence.status === 'offline' && newPresence.status === 'online') {
             // Get the channel to send the message in
-            const channel = client.channels.cache.get('370989106013929474');
+            const channel = client.channels.cache.get('913633377448509481');
         
             // Send the message
             channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696440`);
         }
         else if (oldPresence.status === 'offline' && newPresence.status === 'dnd') {
             // Get the channel to send the message in
-            const channel = client.channels.cache.get('370989106013929474');
+            const channel = client.channels.cache.get('913633377448509481');
         
             // Send the message
             channel.send(`https://imgur.com/a/Bp6FLPA`);
         }
         else if (oldPresence.status === 'offline' && newPresence.status === 'idle') {
             // Get the channel to send the message in
-            const channel = client.channels.cache.get('370989106013929474');
+            const channel = client.channels.cache.get('913633377448509481');
         
             // Send the message
             channel.send(`https://tenor.com/view/stormidle-gif-27216955`);
@@ -39,21 +39,21 @@ client.on('presenceUpdate', (oldPresence, newPresence) => {
 
         else if (oldPresence.status === 'idle' && newPresence.status === 'online') {
             // Get the channel to send the message in
-            const channel = client.channels.cache.get('370989106013929474');
+            const channel = client.channels.cache.get('913633377448509481');
         
             // Send the message
             channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696440`);
         }
         else if (oldPresence.status === 'idle' && newPresence.status === 'dnd') {
             // Get the channel to send the message in
-            const channel = client.channels.cache.get('370989106013929474');
+            const channel = client.channels.cache.get('913633377448509481');
         
             // Send the message
             channel.send(`https://imgur.com/a/Bp6FLPA`);
         }
         else if (oldPresence.status === 'idle' && newPresence.status === 'offline') {
             // Get the channel to send the message in
-            const channel = client.channels.cache.get('370989106013929474');
+            const channel = client.channels.cache.get('913633377448509481');
         
             // Send the message
             channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696441`);
@@ -63,21 +63,21 @@ client.on('presenceUpdate', (oldPresence, newPresence) => {
 
         else if (oldPresence.status === 'dnd' && newPresence.status === 'online') {
             // Get the channel to send the message in
-            const channel = client.channels.cache.get('370989106013929474');
+            const channel = client.channels.cache.get('913633377448509481');
         
             // Send the message
             channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696440`);
         }
         else if (oldPresence.status === 'dnd' && newPresence.status === 'idle') {
             // Get the channel to send the message in
-            const channel = client.channels.cache.get('370989106013929474');
+            const channel = client.channels.cache.get('913633377448509481');
         
             // Send the message
             channel.send(`https://tenor.com/view/stormidle-gif-27216955`);
         }
         else if (oldPresence.status === 'dnd' && newPresence.status === 'offline') {
             // Get the channel to send the message in
-            const channel = client.channels.cache.get('370989106013929474');
+            const channel = client.channels.cache.get('913633377448509481');
         
             // Send the message
             channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696441`);
@@ -87,21 +87,21 @@ client.on('presenceUpdate', (oldPresence, newPresence) => {
 
         else if (oldPresence.status === 'online' && newPresence.status === 'idle') {
             // Get the channel to send the message in
-            const channel = client.channels.cache.get('370989106013929474');
+            const channel = client.channels.cache.get('913633377448509481');
         
             // Send the message
             channel.send(`https://tenor.com/view/stormidle-gif-27216955`);
         }
         else if (oldPresence.status === 'online' && newPresence.status === 'dnd') {
             // Get the channel to send the message in
-            const channel = client.channels.cache.get('370989106013929474');
+            const channel = client.channels.cache.get('913633377448509481');
         
             // Send the message
             channel.send(`https://imgur.com/a/Bp6FLPA`);
         }
         else if (oldPresence.status === 'online' && newPresence.status === 'offline') {
             // Get the channel to send the message in
-            const channel = client.channels.cache.get('370989106013929474');
+            const channel = client.channels.cache.get('913633377448509481');
         
             // Send the message
             channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696441`);
