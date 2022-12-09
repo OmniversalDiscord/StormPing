875af62c7ac449c8bf37c46f033e818e13c2535d
Casey <34421290+MoreOverMedia@users.noreply.github.com>
2022-12-08 21:43:54 -0600

    Update bot.js
    added support for assorted status changes of stormwavz

--- bot.js
+++ bot.js
@@ -11,22 +11,102 @@ client.on('presenceUpdate', (oldPresence, newPresence) => {
     // Check if the user is the one we want to ping
     if (user.id === '256213318060998666') {
 
-      // Check if the user was offline and is now online
-      if (oldPresence.status === 'offline' && newPresence.status === 'online') {
-          // Get the channel to send the message in
-          const channel = client.channels.cache.get('349096556818857995');
+//old status online
 
-          // Send the message
-          channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696440`);
-        } 
-      else if (oldPresence.status === 'online' && newPresence.status === 'offline') {
-          // Get the channel to send the message in
-          const channel = client.channels.cache.get('349096556818857995');
-      
-          // Send the message
-          channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696441`);
+        if (oldPresence.status === 'offline' && newPresence.status === 'online') {
+            // Get the channel to send the message in
+            const channel = client.channels.cache.get('349096556818857995');
+        
+            // Send the message
+            channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696440`);
+        }
+        else if (oldPresence.status === 'offline' && newPresence.status === 'dnd') {
+            // Get the channel to send the message in
+            const channel = client.channels.cache.get('349096556818857995');
+        
+            // Send the message
+            channel.send(`https://media.discordapp.net/attachments/349096556818857995/1050604801077022800/caption.gif`);
+        }
+        else if (oldPresence.status === 'offline' && newPresence.status === 'idle') {
+            // Get the channel to send the message in
+            const channel = client.channels.cache.get('349096556818857995');
+        
+            // Send the message
+            channel.send(`https://media.discordapp.net/attachments/349096556818857995/1050602771323297900/caption.gif`);
+        }
+
+//old status idle
+
+        else if (oldPresence.status === 'idle' && newPresence.status === 'online') {
+            // Get the channel to send the message in
+            const channel = client.channels.cache.get('349096556818857995');
+        
+            // Send the message
+            channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696440`);
+        }
+        else if (oldPresence.status === 'idle' && newPresence.status === 'dnd') {
+            // Get the channel to send the message in
+            const channel = client.channels.cache.get('349096556818857995');
+        
+            // Send the message
+            channel.send(`https://media.discordapp.net/attachments/349096556818857995/1050604801077022800/caption.gif`);
+        }
+        else if (oldPresence.status === 'idle' && newPresence.status === 'offline') {
+            // Get the channel to send the message in
+            const channel = client.channels.cache.get('349096556818857995');
+        
+            // Send the message
+            channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696441`);
+        }
+
+//old status dnd
+
+        else if (oldPresence.status === 'dnd' && newPresence.status === 'online') {
+            // Get the channel to send the message in
+            const channel = client.channels.cache.get('349096556818857995');
+        
+            // Send the message
+            channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696440`);
+        }
+        else if (oldPresence.status === 'dnd' && newPresence.status === 'idle') {
+            // Get the channel to send the message in
+            const channel = client.channels.cache.get('349096556818857995');
+        
+            // Send the message
+            channel.send(`https://media.discordapp.net/attachments/349096556818857995/1050602771323297900/caption.gif`);
+        }
+        else if (oldPresence.status === 'dnd' && newPresence.status === 'offline') {
+            // Get the channel to send the message in
+            const channel = client.channels.cache.get('349096556818857995');
+        
+            // Send the message
+            channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696441`);
+        }
+
+//old status online
+
+        else if (oldPresence.status === 'online' && newPresence.status === 'idle') {
+            // Get the channel to send the message in
+            const channel = client.channels.cache.get('349096556818857995');
+        
+            // Send the message
+            channel.send(`https://media.discordapp.net/attachments/349096556818857995/1050602771323297900/caption.gif`);
+        }
+        else if (oldPresence.status === 'online' && newPresence.status === 'dnd') {
+            // Get the channel to send the message in
+            const channel = client.channels.cache.get('349096556818857995');
+        
+            // Send the message
+            channel.send(`https://media.discordapp.net/attachments/349096556818857995/1050604801077022800/caption.gif`);
+        }
+        else if (oldPresence.status === 'online' && newPresence.status === 'offline') {
+            // Get the channel to send the message in
+            const channel = client.channels.cache.get('349096556818857995');
+        
+            // Send the message
+            channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696441`);
+        }
     }
-  }
 });
 
 require('dotenv').config()
